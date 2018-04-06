fec56fb57cf2228ac7516f7349917bf42ad5cc76
Rikke Petersen <rikkepeterzen@gmail.com>
2018-04-06 11:50:39 +0200

    bt change

--- MeditationVRMaster/Assets/Scripts/BluetoothConnection.cs
+++ MeditationVRMaster/Assets/Scripts/BluetoothConnection.cs
@@ -17,7 +17,21 @@ public class BluetoothConnection : MonoBehaviour
 	void Awake ()
 	{
 		device = new BluetoothDevice ();
-	}
+        if (BluetoothAdapter.isBluetoothEnabled())
+        {
+            connect();
+        }
+        else
+        {
+            //BluetoothAdapter.enableBluetooth(); //you can by this force enabling Bluetooth without asking the user
+            statusText.text = "Status : Please enable your Bluetooth";
+
+            BluetoothAdapter.OnBluetoothStateChanged += HandleOnBluetoothStateChanged;
+            BluetoothAdapter.listenToBluetoothState(); // if you want to listen to the following two events  OnBluetoothOFF or OnBluetoothON
+
+            BluetoothAdapter.askEnableBluetooth();//Ask user to enable Bluetooth
+        }
+    }
 
 	void Start ()
 	{
@@ -26,23 +40,6 @@ public class BluetoothConnection : MonoBehaviour
 		BluetoothAdapter.OnDeviceNotFound += HandleOnDeviceNotFound; //Because connecting using the 'Name' property is just searching, the Plugin might not find it!(only for 'Name').
 	}
 
-	void Update ()
-	{
-		if (Input.GetMouseButtonDown (0)) {
-			if (BluetoothAdapter.isBluetoothEnabled ()) {
-				connect ();
-			} else {
-
-				//BluetoothAdapter.enableBluetooth(); //you can by this force enabling Bluetooth without asking the user
-				statusText.text = "Status : Please enable your Bluetooth";
-
-				BluetoothAdapter.OnBluetoothStateChanged += HandleOnBluetoothStateChanged;
-				BluetoothAdapter.listenToBluetoothState (); // if you want to listen to the following two events  OnBluetoothOFF or OnBluetoothON
-
-				BluetoothAdapter.askEnableBluetooth ();//Ask user to enable Bluetooth
-			}
-		}
-	}
 
 	private void connect ()
 	{
