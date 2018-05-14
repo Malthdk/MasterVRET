9277dfbbdcda4e27418c1aa96f5b8175db814460
Malte Hølledig <malte@holledig.dk>
2018-05-14 10:06:54 +0200

    changes to text file

--- MeditationVRMaster/Assets/Scripts/HandleTextFile.cs
+++ MeditationVRMaster/Assets/Scripts/HandleTextFile.cs
@@ -7,6 +7,7 @@ public class HandleTextFile : MonoBehaviour
 {
 	public EEGListener eeg;
 	public Calibration cali;
+	private float delta, theta, lowAlpha, highAlpha, lowBeta, highBeta, lowGamma, highGamma;
 	private string startTime;
 	private TextWriter eegWriter;
 	private int seconds;
@@ -29,9 +30,46 @@ public class HandleTextFile : MonoBehaviour
 
 	IEnumerator WriteData() {
 		seconds++;
-		eegWriter.WriteLine(seconds + ", " + eeg.PoorSignal + ", " + eeg.Attention + ", " + eeg.Meditation + ", " + eeg.Zone + ", " + eeg.Delta + ", " + eeg.Theta + ", " + eeg.LowAlpha + ", " + eeg.HighAlpha + ", " + eeg.LowBeta + ", " + eeg.HighBeta + ", " + eeg.LowGamma + ", " + eeg.HighGamma + ", " + eeg.Raw);
+		eegWriter.WriteLine(seconds + ", " + eeg.PoorSignal + ", " + eeg.Attention + ", " + eeg.Meditation + ", " + eeg.Zone + ", " + delta + ", " + theta + ", " + lowAlpha + ", " + highAlpha + ", " + lowBeta + ", " + highBeta + ", " + lowGamma + ", " + highGamma + ", " + eeg.Raw);
 		yield return new WaitForSeconds (1f);
-		StartCoroutine("WriteData");
+		StartCoroutine ("WriteData");
+		StartCoroutine("CalcAverage");
+	}
+
+	IEnumerator CalcAverage() {
+		float delta_ = 0; 
+		float theta_ = 0; 
+		float lowAlpha_ = 0;
+		float highAlpha_ = 0;
+		float lowBeta_ = 0;
+		float highBeta_ = 0;
+		float lowGamma_ = 0;
+		float highGamma_ = 0;
+		int counter = 0;
+
+		float t = 0;
+		while (t < 1f) {
+			t += Time.deltaTime;
+			delta_ += eeg.Delta; 
+			theta_ += eeg.Theta; 
+			lowAlpha_ += eeg.LowAlpha;
+			highAlpha_ += eeg.HighAlpha;
+			lowBeta_ += eeg.LowBeta;
+			highBeta_ += eeg.HighBeta;
+			lowGamma_ += eeg.LowGamma;
+			highGamma_ += eeg.HighGamma;
+			counter++;
+			yield return null;
+		}
+
+		delta = delta_ / counter; 
+		theta = theta_ / counter; 
+		lowAlpha = lowAlpha_ / counter;
+		highAlpha = highAlpha_ / counter;
+		lowBeta = lowBeta_ / counter;
+		highBeta = highBeta_ / counter;
+		lowGamma = lowGamma_ / counter;
+		highGamma = highGamma_ / counter;
 	}
 
 	void OnApplicationQuit() {
